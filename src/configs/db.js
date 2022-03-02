18b4eb4547b489969dea77482b8849216536c015
Achintya Gaumat <30311965+vibrantachintya@users.noreply.github.com>
2022-03-03 00:36:45 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-    return mongoose.connect("mongodb+srv://achintya:db1234@cluster0.cga5s.mongodb.net/library?retryWrites=true&w=majority");
+    return mongoose.connect("mongodb+srv://achintya:@cluster0.cga5s.mongodb.net/library?retryWrites=true&w=majority");
 };
 
 module.exports = connect;
